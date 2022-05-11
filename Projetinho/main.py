1451063031d87c38c6636e59aa6799ade8f7b314
SocioPJ <20.00704-3@maua.br>
2022-05-11 17:51:49 -0300

    funcionando

--- Projetinho/main.py
+++ Projetinho/main.py
@@ -1,3 +1,31 @@
+def simular_dfa(dfa,entrada):
+    estado = dfa['initial_state']
+    aceitar = False
+    while len(entrada) > 0:
+        simbolo = entrada.pop(0)
+        if simbolo not in dfa['sigma']:
+            print('\nSimbolo nao pertence ao alfabeto do automato')
+            print('\n========================================================================================')
+            break
+        elif estado not in dfa['states']:
+            print('\nEstado nao pertence ao conjunto de estados do automato')
+            print('\n========================================================================================')
+            break
+        try:
+            proximo_estado = dfa['delta'][(estado,simbolo)]
+            print('Estado: ' + str(estado) + ' Simbolo: ' + str(simbolo) + ' Proximo estado: ' + str(proximo_estado))
+            estado = proximo_estado
+        except KeyError:
+            print('Não foi possivel realizar a transição entre estados')
+
+    if estado in dfa['final_states']:
+        aceitar = True
+    if aceitar == True:
+        print('A cadeia foi aceita pelo automato')
+    else:
+        print('\nA cadeia nao foi aceita pelo automato')
+
+
 data_file = input("Digite o nome do arquivo: ")
 cadeia = input('Digite a cadeia: ')
 cadeia = list(cadeia)
@@ -12,34 +40,7 @@ dfa = eval(dfa_data)
 print('========================================================================================')
 
 
-def simular_dfa(dfa,entrada):
-    estado = dfa['initial_state']
-    aceitar = False
-    while len(entrada) >= 0:
-        simbolo = entrada.pop(0)
-        if simbolo not in dfa['sigma']:
-            print('\nSimbolo nao pertence ao alfabeto do automato')
-            print('\n========================================================================================')
-            break
-        elif estado not in dfa['states']:
-            print('\nEstado nao pertence ao conjunto de estados do automato')
-            print('\n========================================================================================')
-            break
-        proximo_estado = dfa['delta'][estado][dfa['sigma'].index(str(simbolo))]
-        print('Estado: ' + str(estado) + ' Simbolo: ' + str(simbolo) + ' Proximo estado: ' + str(proximo_estado))
-        estado = proximo_estado
-        
-        if dfa['delta',estado] not in dfa['delta']:
-            print('Nao foi possivel realizar transicao entre os estados')
-            
-    
-    if estado in dfa['final_states'] and len(entrada) == 0:
-        aceitar = True
 
-    if aceitar == True:
-        print('A cadeia foi aceita pelo automato')
-    else:
-        print('\nA cadeia nao foi aceita pelo automato')
 
 
 simular_dfa(dfa,cadeia)
